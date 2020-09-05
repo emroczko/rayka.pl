0415d85e53dfdf27732ab406f8aeab21ff392b91
emroczko <57818231+emroczko@users.noreply.github.com>
2020-09-05 02:16:17 +0200

    added welcome gallery (javascript)

--- RAYKA.js
+++ RAYKA.js
@@ -1,3 +1,14 @@
+var interval = setInterval(photosChange, 3000)
+var index = 1;
+
 function photosChange() {
-    
+    photos = ["photo1.png", "photo2.png", "photo3.png"]
+    document.getElementsByClassName("welcomephoto")[0].style.backgroundImage = "url(" + photos[index] +")";
+    if(index < 2){
+        index++;
+    }
+    else{
+        index = 0;
+    }
 }
+
