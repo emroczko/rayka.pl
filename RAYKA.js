3996d0e627f5bb7299ba8c915e363c6558583c5b
emroczko <57818231+emroczko@users.noreply.github.com>
2020-09-06 02:58:08 +0200

    photogallery

--- RAYKA.js
+++ RAYKA.js
@@ -4,6 +4,14 @@ var index = 1;
 function photosChange() {
     photos = ["photo1.png", "photo2.png", "photo3.png"]
     document.getElementsByClassName("welcomephoto")[0].style.backgroundImage = "url(" + photos[index] +")";
+    //document.getElementsByClassName("welcomephoto")[0].style.transition = "0.4s all";
+    if(index == 1)
+    {
+        document.getElementsByClassName("welcomephoto")[0].style.color = "#262626";
+    }
+    else{
+        document.getElementsByClassName("welcomephoto")[0].style.color = "#aaa";
+    }
     if(index < 2){
         index++;
     }
