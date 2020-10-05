d1a72df134624291f110faa828be57760507ee19
emroczko <57818231+emroczko@users.noreply.github.com>
2020-10-05 17:37:52 +0200

    update server.php

--- server.php
+++ server.php
@@ -2,10 +2,28 @@
 
 $myemail = "kontakt@rayka.pl";
 
+$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+$recaptcha_secret = 'secret code not available';
+$recaptcha_response = $_POST['recaptcha_response'];
 
-//if(isset($_POST['email']) && isset($_POST['message']) ){
-    $from = $_POST['email'];
-    $text = $_POST['message'];
-    mail($myemail, "Temat", $text, $from);
-//}
+$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+$recaptcha = json_decode($recaptcha);
+
+if ($recaptcha->success == true && $recaptcha->score >= 0.5 && $recaptcha->action == 'contact') {
+    
+    $from = $_POST["email"];
+    $text = $_POST["message"];
+    mail($myemail, "Temat", $text, $from);  
+    $success_output = "Twoja wiadomość została wysłana!";
+} else {
+    $error_output = "Coś poszło nie tak. Spróbuj ponownie.";
+}
+
+$output = array(
+    'error'     =>  $error_output,
+    'success'   =>  $success_output
+);
+
+echo json_encode($output);
+
 ?>
